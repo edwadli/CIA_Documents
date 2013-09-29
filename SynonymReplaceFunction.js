7a8760eb9ada7acf834d867bdd20dcfaa2078c3f
Edward Li <el2724@columbia.edu>
2013-09-29 02:54:24 -0400

    added a temporary HTMLreplaceWords file

--- SynonymReplaceFunction.js
+++ SynonymReplaceFunction.js
@@ -4,16 +4,26 @@
 
 $(document).ready(function(){
 
+	//
+	bigList=masterList;
+	//
 
-	var syno = bigList[i][0];
-	var synPos = $("body").text().toLowerCase().indexOf(syno);
-
-	$("body").text(
-		$(this).text().replace(
-			$("body").text().substr(synPos,syno.length),
-									bigList[i][random(#SATwords)]
-									)
-		);
-
+	for(var i = 0;i<bigList.length;i++){
+		console.log("x");
+		var syno = bigList[i][0];
+		var synPos = $("body").text().toLowerCase().indexOf(" " + syno + " ");
+		if (synPos !== -1){
+			var ED = 1 + Math.floor(Math.random() * bigList[i].length-1);
+			$("body").text($("body").text().replace($('body').text().substr(synPos + 1,syno.length),bigList[i][ED]));
+		// $("body").text(
+		// 	$(this).text().replace(
+		// 		$("body").text().substr(synPos,syno.length),
+		// 							bigList[i][Math.floor(
+		// 									Math.random()
+		// 									*bigList[i].length)]
+		// 								)
+		// );
+		}
+	}
 
 });
