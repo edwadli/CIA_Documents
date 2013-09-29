2e51274d327e575e8f9fba60fcda97f4e80f628e
brian kim <bck2116@columbia.edu>
2013-09-29 07:28:25 -0400

    the chrome extension kinda working

--- HTMLSynonymReplaceFunction.js
+++ HTMLSynonymReplaceFunction.js
@@ -7,12 +7,14 @@ $(document).ready(function(){
 	//
 	bigList=masterList;
 	//
-
-	for(var i = 0;i<bigList.length;i++){
+	var i = Math.floor(Math.random()*10);
+	for(;i<bigList.length;i=i+10){
+		console.log("x");
 		var syno = bigList[i][0];
 		var synPos = $("body").html().toLowerCase().indexOf(" " + syno + " ");
-		if (synPos !== -1){
-
+		re = new RegExp('<p>' + syno + '<p>', 'g')
+		if(re.test($("body").html())){
+		//if (synPos !== -1){
 			var ED = 1 + Math.floor(Math.random() * (bigList[i].length-1));
 			$("body").html($("body").html().replace($('body').html().substr(synPos + 1,syno.length),"<span data-tooltip class='has-tip' title=" + syno + ">" + bigList[i][ED] + "</span>"));
 		}
