3388a5b52dbdd5dd7231a571bb0d630c3ee6c98a
Edward Li <el2724@columbia.edu>
2013-09-29 04:49:38 -0400

    everything works!!!

--- HTMLSynonymReplaceFunction.js
+++ HTMLSynonymReplaceFunction.js
@@ -12,16 +12,9 @@ $(document).ready(function(){
 		var syno = bigList[i][0];
 		var synPos = $("body").html().toLowerCase().indexOf(" " + syno + " ");
 		if (synPos !== -1){
-<<<<<<< HEAD
-			var ED = Math.floor(1 + Math.random() * bigList[i].length-1);
-			console.log(bigList[i].length + syno + ": " + bigList[i][ED]);
-			if(bigList[i][ED]!==syno){
-				$("body").html($("body").html().replace($('body').html().substr(synPos + 1,syno.length),bigList[i][ED]));
-			}
-=======
+
 			var ED = 1 + Math.floor(Math.random() * (bigList[i].length-1));
 			$("body").html($("body").html().replace($('body').html().substr(synPos + 1,syno.length),"<span data-tooltip class='has-tip' title=" + syno + ">" + bigList[i][ED] + "</span>"));
->>>>>>> 9f8d9ed1396f44790bf1b3462415080ae9c5e893
 		}
 	}
 
